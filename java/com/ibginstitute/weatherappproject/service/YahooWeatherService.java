b1cb5e1af8c10f6a1f4e78d0e61484edaed7ca61
ricardogwill <ricardogwill@gmail.com>
2018-10-30 15:30:52 -0400

    Changed celsius to fahrenheit (..."and u='f'").

--- java/com/ibginstitute/weatherappproject/service/YahooWeatherService.java
+++ java/com/ibginstitute/weatherappproject/service/YahooWeatherService.java
@@ -34,7 +34,7 @@ public class YahooWeatherService {
             @Override
             protected String doInBackground(String... strings) {
 
-                String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\") and u='f'", strings[0]);
+                String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\") and u='c'", strings[0]);
 
                 String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
 
